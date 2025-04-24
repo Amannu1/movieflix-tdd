255bf648c467e57cb560df36f8b9b46f64888d44
Luiz <manuercolle@gmail.com>
2025-04-24 14:49:09 -0300

    Genre authentication

--- src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -4,6 +4,7 @@ import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.services.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class GenreController {
     private GenreService service;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_MEMBER', 'ROLE_VISITOR')")
     public ResponseEntity<List<GenreDTO>> findAll(){
         List<GenreDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
